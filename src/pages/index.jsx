f99d2de1296b4ef0e05b29e86f067cf169b14c2b
github naoki4287 <github@waka.naoki86@icloud.com>
2022-02-28 13:41:34 +0900

    ⚡️ useCallback, useEffectの第二引数に関して説明しました

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,21 +4,17 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useEffect, useState } from "react";
-
-// const handleClick = (e) => {
-//   console.log(e.target.href);
-//   e.preventDefault();
-// }
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   //分割代入
   const [foo, setFoo] = useState(1)
 
-  const handleClick = (e) => {
-    setFoo((foo) => foo + 1);
-    setFoo((foo) => foo + 1);
-  };
+  const handleClick = useCallback(() => {
+    if (foo < 10) {
+      setFoo((foo) => foo + 1);
+    }
+  }, [foo]);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue"
