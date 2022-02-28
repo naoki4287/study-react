239fe95951b1f322580b855a70ba46cf6e9ae01e
github naoki4287 <github@waka.naoki86@icloud.com>
2022-02-28 09:24:37 +0900

    ⚗️  不要な記述でいつか消しますが、試しにクリックイベントを書いてみました。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,34 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from 'src/styles/Home.module.css'
-import { Footer  } from 'src/components/Footer'
-import { Main } from 'src/components/Main'
-import { Header } from 'src/components/Header'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "src/styles/Home.module.css";
+import { Footer } from "src/components/Footer";
+import { Main } from "src/components/Main";
+import { Header } from "src/components/Header";
+import { useCallback } from "react";
+
+// const handleClick = (e) => {
+//   console.log(e.target.href);
+//   e.preventDefault();
+// }
 
 export default function Home() {
+  const foo = 1;
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
-        <title>Index Page</title> 
+        <title>Index Page</title>
       </Head>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
       <Footer />
     </div>
-  )
+  );
 }
