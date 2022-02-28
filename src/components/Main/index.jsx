f8eef02930786c72aef1105a7bec1293864b6f2e
github naoki4287 <github@waka.naoki86@icloud.com>
2022-02-28 10:15:15 +0900

    ✨ 講座用のuseEffectを導入

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,8 +1,10 @@
 import styles from 'src/components/Main/Main.module.css'
 import { Headline } from 'src/components/Headline'
 import { Links } from 'src/components/Links'
+import { useEffect } from 'react'
 
 export function Main(props) {
+
   return (
       <main className={styles.main}>
         <Headline page={props.page}>
