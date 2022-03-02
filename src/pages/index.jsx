df627288882cf7b3fa392f71996595bf0f428ff9
github naoki4287 <github@waka.naoki86@icloud.com>
2022-03-02 09:34:13 +0900

    ✨ コンポーネント間のStateのリフトアップについて勉強

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,9 +3,7 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCounter } from "src/hooks/useCounter";
-import { useInputArray } from "src/hooks/useInputArray";
-import { useBgLightBlue } from "src/hooks/useBgLightBlue";
+
 
 
 
