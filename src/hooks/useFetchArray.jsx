b53055732e544a57d563b6f1cfa8f332e2763f77
github naoki4287 <github@waka.naoki86@icloud.com>
2022-03-13 12:25:02 +0900

    ♻️ コードを見やすいように並び替え

--- src/hooks/useFetchArray.jsx
+++ src/hooks/useFetchArray.jsx
@@ -14,22 +14,27 @@ const useFetchArray = (url) => {
 
 const API_URL = "https://jsonplaceholder.typicode.com";
 
-export const useComments = () => {
-  return useFetchArray(`${API_URL}/comments`, fetcher);
-};
 
+// posts
 export const usePosts = () => {
   return useFetchArray(`${API_URL}/posts`, fetcher);
 };
 
+export const usePostsByUserId = (id) => {
+  return useFetchArray(id ? `${API_URL}/posts?userId=${id}` : null, fetcher);
+};
+
+// users
 export const useUsers = () => {
   return useFetchArray(`${API_URL}/users`, fetcher);
+};
+
+// comments
+export const useComments = () => {
+  return useFetchArray(`${API_URL}/comments`, fetcher);
 };
 
 export const useCommentsByPostId = (id) => {
   return useFetchArray(id ? `${API_URL}/comments?postId=${id}` : null, fetcher);
 };
 
-export const usePostsByUserId = (id) => {
-  return useFetchArray(id ? `${API_URL}/posts?userId=${id}` : null, fetcher);
-};
